f56634b0aa6568f04bd9b28af90eab4b98d5123f
Malthe Borch <mborch@gmail.com>
2009-09-24 14:03:59 +0000

    The layer field was just app-level sugar on a global object (register the layer with the application framework). We definitely don't want to engage in that (and it's deprecated, appearently).

--- z3c/jbot/metadirectives.py
+++ z3c/jbot/metadirectives.py
@@ -1,6 +1,6 @@
 from zope.interface import Interface
 from zope.configuration import fields
-from zope.app.component.back35 import LayerField
+from zope.configuration.fields import GlobalObject
 
 class ITemplateOverridesDirective(Interface):
     """Directive which registers a directory with template overrides."""
@@ -9,7 +9,7 @@ class ITemplateOverridesDirective(Interface):
         title=u"Path to directory",
         required=True)
 
-    layer = LayerField(
+    layer = GlobalObject(
         title=u"The layer the overrides should be enabled for",
         description=u"By default overrides are used for all layers.",
         required=False
