710f22a93c166e43cd4f81eb4033df838e24d05f
Hanno Schlichting <hanno@hannosch.eu>
2013-03-02 12:43:33 +0100

    post-git cleanup

--- setup.py
+++ setup.py
@@ -1,36 +1,39 @@
 from setuptools import setup, find_packages
 
-version = '0.7.1'
+__version__ = '0.7.1'
 
-setup(name='z3c.jbot',
-      version=version,
-      description="Drop-in template overrides.",
-      long_description="\n".join(
-          (open("README.txt").read(), open("CHANGES.txt").read())),
-      classifiers=[
+setup(
+    name='z3c.jbot',
+    version=__version__,
+    description="Drop-in template overrides.",
+    long_description=(open('README.rst').read() + "\n" +
+                      open('CHANGES.rst').read()),
+    classifiers=[
         "Framework :: Zope2",
         "Framework :: Zope3",
         "Programming Language :: Python",
-        ],
-      keywords='page template override',
-      author='Malthe Borch',
-      author_email='mborch@gmail.com',
-      url='',
-      license='ZPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['z3c'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          'zope.pagetemplate',
-          'zope.component',
-          'zope.configuration',
-          'zope.security',
-          'zope.publisher',
-      ],
-      entry_points="""
-      [z3c.autoinclude.plugin]
-      target = plone
-      """,
-      )
+    ],
+    keywords='page template override',
+
+    author='Zope Foundation and Contributors',
+    author_email='zope-dev@zope.org',
+    url='http://pypi.python.org/pypi/z3c.jbot',
+    license='ZPL 2.1',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    namespace_packages=['z3c'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'setuptools',
+        'zope.pagetemplate',
+        'zope.component',
+        'zope.configuration',
+        'zope.security',
+        'zope.publisher',
+    ],
+    entry_points="""
+    [z3c.autoinclude.plugin]
+    target = plone
+    """,
+)
