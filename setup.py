894abfd8f2047e24ea27d0e1741bbd2d8fc117bf
Maurits van Rees <maurits@vanrees.org>
2018-11-07 10:56:50 +0100

    Fixed setup.py url.
    [ci skip]

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     keywords='page template override',
     author='Zope Foundation and Contributors',
     author_email='zope-dev@zope.org',
-    url='http://pypi.python.org/pypi/z3c.jbot',
+    url='https://pypi.org/project/z3c.jbot',
     license='ZPL 2.1',
     packages=find_packages('src'),
     package_dir={'': 'src'},
