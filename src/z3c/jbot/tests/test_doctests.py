ebec5fd677aa8e9550e3f44771def6cf23f1e3b3
Jens W. Klein <jk@kleinundpartner.at>
2020-08-07 13:03:31 +0200

    fix test to run on windows with windows line endings (2)

--- src/z3c/jbot/tests/test_doctests.py
+++ src/z3c/jbot/tests/test_doctests.py
@@ -10,18 +10,27 @@ import zope.interface
 from .common import setUp
 from .common import tearDown
 
-OPTIONFLAGS = (doctest.ELLIPSIS |
-               doctest.NORMALIZE_WHITESPACE)
+OPTIONFLAGS = (
+    doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE
+)
 
 
 class Py23DocChecker(doctest.OutputChecker):
     def check_output(self, want, got, optionflags):
+        # fix windows line-endings to match ones in tests
+        got = got.replace(r"\r\n", r"\n")
+        # fix binary/unicode differences between python versions
         if six.PY2:
             want = re.sub("b'(.*?)'", "'\\1'", want)
         else:
             want = re.sub("u'(.*?)'", "'\\1'", want)
-        want = want.replace(r"\r\n", r"\n")
         return doctest.OutputChecker.check_output(self, want, got, optionflags)
+
+    def output_difference(self, example, got, optionflags):
+        # fix windows line-endings to match ones in tests
+        got = got.replace(r"\r\n", r"\n")
+        return doctest.OutputChecker.output_difference(
+            self, example, got, optionflags)
 
 
 def test_suite():
