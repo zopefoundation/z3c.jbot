c8497bcf61caf7b15ca90bd71970e17e8a178a29
Michael Howitz <icemac@gmx.net>
2025-04-09 08:39:29 +0200

    Switch to PEP 420 native namespace.

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
 
@@ -27,9 +26,6 @@ setup(
     author_email='zope-dev@zope.dev',
     url='https://github.com/zopefoundation/z3c.jbot',
     license='ZPL-2.1',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    namespace_packages=['z3c'],
     include_package_data=True,
     zip_safe=False,
     python_requires='>=3.9',
